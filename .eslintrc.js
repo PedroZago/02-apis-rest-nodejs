abda763d0658dd16a341ff8c0e6b83a7e8ff3bb3
Pedro de Camargo Zago <pedro.camargozago@hotmail.com.br>
2023-02-22 19:11:26 -0300

    Estrutura da aplicação

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: [
+    'standard',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  settings: {
+    'import/parsers': {
+      [require.resolve(
+        '@typescript-eslint/parser',
+      )]: ['.ts', '.tsx', '.d.ts'],
+    },
+  },
+};
