2602b93b1ee4cc489c33ff4ba3756915c2938c13
Pedro de Camargo Zago <pedro.camargozago@hotmail.com.br>
2023-02-23 22:40:48 -0300

    Banco de Dados

---
+++ src/database.ts
@@ -0,0 +1,19 @@
+import {
+  knex as setupKnex,
+  Knex,
+} from 'knex';
+import { env } from './env';
+
+export const config = {
+  client: 'sqlite',
+  connection: {
+    filename: env.DATABASE_URL,
+  },
+  useNullAsDefault: true,
+  migrations: {
+    extension: 'ts',
+    directory: './db/migrations',
+  },
+} as Knex.Config;
+
+export const knex = setupKnex(config);
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,49 @@
 import fastify from 'fastify';
+import { knex } from './database';
+import crypto from 'node:crypto';
+import { env } from './env';
 
 const app = fastify();
 
-app.get('/hello', () => {
-  return 'Hello world!';
+app.post('/transaction', async () => {
+  const transaction = await knex(
+    'transactions',
+  )
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Transação de teste',
+      amount: 1000,
+    })
+    .returning('*');
+
+  return transaction;
 });
 
-app.listen({ port: 3333 }).then(() => {
-  console.log('HTTP Server Running...');
+app.get('/transaction', async () => {
+  const transactions = await knex(
+    'transactions',
+  ).select('*');
+
+  return transactions;
 });
+
+app.get(
+  '/transaction/amount',
+  async () => {
+    const transactions = await knex(
+      'transactions',
+    )
+      .where('amount', 1000)
+      .select('*');
+
+    return transactions;
+  },
+);
+
+app
+  .listen({ port: env.PORT })
+  .then(() => {
+    console.log(
+      'HTTP Server Running...',
+    );
+  });
