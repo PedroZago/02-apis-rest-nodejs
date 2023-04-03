ebd0744d5d30073b0d0899f8bfd6377b54159a9d
Pedro de Camargo Zago <pedro.camargozago@hotmail.com.br>
2023-04-03 18:55:07 -0300

    Build

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,10 @@
 module.exports = {
-  extends: [
-    'standard',
-    'plugin:prettier/recommended',
-  ],
+  extends: ['standard', 'plugin:prettier/recommended'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
   settings: {
     'import/parsers': {
-      [require.resolve(
-        '@typescript-eslint/parser',
-      )]: ['.ts', '.tsx', '.d.ts'],
+      [require.resolve('@typescript-eslint/parser')]: ['.ts', '.tsx', '.d.ts'],
     },
   },
 };
